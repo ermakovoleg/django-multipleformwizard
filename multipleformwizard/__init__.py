52359b36646b8c50b3467b02e826a0cc714fe61b
Dirk Moors <dirkmoors@gmail.com>
2015-03-09 14:40:58 +0100

    Bugfix

--- multipleformwizard/__init__.py
+++ multipleformwizard/__init__.py
@@ -1,4 +1,9 @@
-__version__ = '0.2.7'
+__version__ = '0.2.8'
 
-from .views import (SessionMultipleFormWizardView, CookieMultipleFormWizardView, NamedUrlSessionMultipleFormWizardView,
-                    NamedUrlCookieMultipleFormWizardView, MultipleFormWizardView, NamedUrlMultipleFormWizardView)
+try:
+    # This is in a try-except block to prevent import errors at install time
+    from .views import (SessionMultipleFormWizardView, CookieMultipleFormWizardView,
+                        NamedUrlSessionMultipleFormWizardView, NamedUrlCookieMultipleFormWizardView,
+                        MultipleFormWizardView, NamedUrlMultipleFormWizardView)
+except ImportError:
+    pass
