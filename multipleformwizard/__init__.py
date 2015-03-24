c38061a49f42323d526aa26fff0632de2fe93616
Dirk Moors <dirkmoors@gmail.com>
2015-03-24 08:42:38 +0100

    Bugfix

--- multipleformwizard/__init__.py
+++ multipleformwizard/__init__.py
@@ -1,9 +1,11 @@
-__version__ = '0.2.12'
+from django.core.exceptions import ImproperlyConfigured
+
+__version__ = '0.2.13'
 
 try:
     # This is in a try-except block to prevent import errors at install time
     from .views import (SessionMultipleFormWizardView, CookieMultipleFormWizardView,
                         NamedUrlSessionMultipleFormWizardView, NamedUrlCookieMultipleFormWizardView,
                         MultipleFormWizardView, NamedUrlMultipleFormWizardView)
-except ImportError:
+except (ImportError, ImproperlyConfigured):
     pass
